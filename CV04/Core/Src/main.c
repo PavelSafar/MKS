fd02593429dfd3b9e259a912a7a8d35fab9d776e
safar.pavel1@seznam.cz <BG7S-F1G2-bg7s-f1g2>
2020-10-25 19:55:39 +0100

    2_3 exponencialni kumulace

--- CV04/Core/Src/main.c
+++ CV04/Core/Src/main.c
@@ -32,6 +32,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define ADC_Q 12
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -52,9 +53,12 @@ static void MX_GPIO_Init(void);
 static void MX_ADC_Init(void);
 /* USER CODE BEGIN PFP */
 static volatile uint32_t raw_pot;
+static uint32_t avg_pot;
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
 {
- raw_pot = HAL_ADC_GetValue(hadc);
+	raw_pot = avg_pot >> ADC_Q;
+	avg_pot -= raw_pot;
+	avg_pot += HAL_ADC_GetValue(hadc);
 }
 /* USER CODE END PFP */
 
