cdda21901f6d664eae20ad6281692cb69d40fb72
safar.pavel1@seznam.cz <BG7S-F1G2-bg7s-f1g2>
2020-11-12 21:52:46 +0100

    CV06 DS18B20

---
+++ CV06/Core/Src/sct.c
@@ -0,0 +1,48 @@
+/*
+ * sct.c
+ *
+ *  Created on: 16. 10. 2020
+ *      Author: Pavel
+ */
+
+#include "main.h"
+#include "STM32F0xx.h"
+
+#include "sct.h"
+
+
+void sct_led(uint32_t value)
+{
+	for (uint8_t j = 0; j<32; j++)
+	{
+		HAL_GPIO_WritePin(GPIOB,SCT_SDI_Pin,value&1);
+		value=value>>1;
+		HAL_GPIO_WritePin(GPIOB, SCT_CLK_Pin, 1);
+		HAL_GPIO_WritePin(GPIOB, SCT_CLK_Pin, 0);
+	}
+	HAL_GPIO_WritePin(GPIOB, SCT_NLA_Pin, 1);
+	HAL_GPIO_WritePin(GPIOB, SCT_NLA_Pin, 0);
+}
+
+void sct_init(void)
+{
+	sct_led(0);
+}
+
+void sct_value(uint16_t value, uint16_t bargraph)
+{
+	value = value%1000;
+	uint32_t out = reg_values[2][value%10];
+	value = value /10;
+	if(value>0)
+	{
+		out |= reg_values[1][value%10];
+		value = value /10;
+		if(value>0)
+		{
+			out |= reg_values[0][value%10];
+		}
+	}
+	out |= reg_values[3][bargraph%10];
+	sct_led(out);
+}
